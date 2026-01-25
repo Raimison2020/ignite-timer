a13f61a96e8f8f7cfd34a4e48c53e662971d5f5f
Raimison <raimison.ti@gmail.com>
2026-01-25 09:29:48 -0400

    Configuração ThemeProvider, createGlobalStyle e tipagem de temas styled.d.ts

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,20 @@
+import { ThemeProvider } from "styled-components";
 import { Button } from "./components/Button";
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secondary" />
       <Button variant="success" />
       <Button variant="danger" />
       <Button />
-    </>
+
+      <h1>Texto de Exemplo</h1>
+
+      <GlobalStyle />
+    </ThemeProvider>
   )
 }
